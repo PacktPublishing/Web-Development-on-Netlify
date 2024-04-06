3f7b6953fdd16253e6b95aade94549ebf6b7022c
Ekene Eze <ekeneeze3@gmail.com>
2024-04-07 00:13:14 +0400

    Update odb-welcome.js

--- chapter3/odb-welcome.js
+++ chapter3/odb-welcome.js
@@ -1,16 +1,15 @@
 import { builder } from "@netlify/functions";
+const myHandler = async (event) => {
+  const segments = event.path.split('/');
+  const name = segments[segments.length - 1];
+  const greeting = name ? `Welcome ${name}` : 'Hello World';
 
-const myHandler = async (event, context) => {
-    const segments = event.path.split('/');
-    const name = segments[segments.length - 1];
-    const greeting = name ? `Hello ${name}` : 'Hello World';
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Content-Type": "text/html",
-        },
-        body: `
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: `
       <!DOCTYPE html>
       <html>
         <body>
@@ -18,8 +17,7 @@ const myHandler = async (event, context) => {
         </body>
     </html>
     `,
-    };
+  };
 };
 const handler = builder(myHandler);
-
 export { handler };
