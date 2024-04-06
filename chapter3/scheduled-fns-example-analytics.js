dc26e339c8c96265ae0be077094ecffb2af07247
Ekene Eze <ekeneeze3@gmail.com>
2024-04-07 00:10:12 +0400

    Update scheduled-fns-example-analytics.js

--- chapter3/scheduled-fns-example-analytics.js
+++ chapter3/scheduled-fns-example-analytics.js
@@ -1,21 +1,15 @@
-import { scheduled } from '@netlify/functions';
-import { fetch } from 'node-fetch';
-
-async function handler() {
+export default async (req) => {
     // Fetch the latest analytics data
     const response = await fetch(`https://analytics.example.com/data`);
     const analyticsData = await response.json();
-
     // Update the site's analytics database
     await updateAnalyticsDatabase(analyticsData);
 }
-
 const updateAnalyticsDatabase = async (data) => {
-    // Logic to update the database here
+    // Logic to update the database here 
     console.log('Updating analytics database with data:', data);
 }
-
-export default scheduled(handler, {
-    // Every Monday at 10 AM
-    schedule: '0 10 * * 1',
-});
+export const config = {
+    // Every monday @ 10am
+    Schedule: '0 10 * * 1' || '@weekly'
+}
